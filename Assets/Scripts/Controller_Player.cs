d7e35de737a9da921d34d8c0b9fcd8ee3fb2f15d
MatMaku <matiasd.quaremba@gmail.com>
2024-06-10 18:23:27 -0300

    Apuntado y disparo
    Arregle los sprites para que todos coincidan y agregue la mecanica de apuntar y disparar con sus respectivas animaciones

--- Assets/Scripts/Controller_Player.cs
+++ Assets/Scripts/Controller_Player.cs
@@ -6,7 +6,9 @@ public class Controller_Player : MonoBehaviour
 {
     public float Speed = 1f;
 
-    private bool ContraPared;
+    private bool Apuntando = false;
+    private bool Disparando = false;
+    private bool SePuedeMover = true;
 
     private Rigidbody2D Rigidbody2D;
     private SpriteRenderer SpriteRenderer;
@@ -22,10 +24,36 @@ public class Controller_Player : MonoBehaviour
 
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        if (Input.GetButton("Fire2"))
+        {
+            MoveInput = new Vector2(0, 0);
+            Apuntando = true;
+            SePuedeMover = false;
 
-        MoveInput = new Vector2(moveX, moveY).normalized;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Disparando = true;
+                StartCoroutine(ShootWait());
+            }
+        }
+        else
+        {
+            if (Apuntando && !Disparando)
+            {
+                Apuntando = false;
+
+                StartCoroutine(AimWait());
+            }
+
+            if (SePuedeMover)
+            {
+
+                float moveX = Input.GetAxisRaw("Horizontal");
+                float moveY = Input.GetAxisRaw("Vertical");
+
+                MoveInput = new Vector2(moveX, moveY).normalized;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -41,6 +69,17 @@ public class Controller_Player : MonoBehaviour
 
     private void Animación()
     {
+        if (MoveInput.x < 0)
+        {
+            Animator.SetLayerWeight(0, 0);
+            Animator.SetLayerWeight(1, 1);
+        }
+        else if (MoveInput.x > 0)
+        {
+            Animator.SetLayerWeight(0, 1);
+            Animator.SetLayerWeight(1, 0);
+        }
+
         if (MoveInput != new Vector2(0, 0))
         {
             Animator.SetBool("EnMovimiento", true);
@@ -48,15 +87,36 @@ public class Controller_Player : MonoBehaviour
         else
         {
             Animator.SetBool("EnMovimiento", false);
+        }   
+
+        if (Apuntando)
+        {
+            Animator.SetBool("Apuntando", true);
+        }
+        else
+        {
+            Animator.SetBool("Apuntando", false);
         }
 
-        if (MoveInput.x < 0)
+        if (Disparando)
         {
-            SpriteRenderer.flipX = true;
+            Animator.SetBool("Disparando", true);
         }
-        else if (MoveInput.x > 0)
+        else
         {
-            SpriteRenderer.flipX = false;
+            Animator.SetBool("Disparando", false);
         }
+    }
+
+    private IEnumerator AimWait()
+    {
+        yield return new WaitForSeconds(.3f);
+        SePuedeMover = true;
+    }
+
+    private IEnumerator ShootWait()
+    {
+        yield return new WaitForSeconds(.1f);
+        Disparando = false;
     }
 }
